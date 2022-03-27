a543755f8c1d539ac0b1b8375f23e8feb8693028
Samson Achiaga <sirhumble84@gmail.com>
2022-03-27 17:33:26 +0100

    Jetpack Compose set up

--- app/src/main/java/com/certified/schoolnotes/ui/MainActivity.kt
+++ app/src/main/java/com/certified/schoolnotes/ui/MainActivity.kt
@@ -1,38 +1,64 @@
-/*
- * Copyright (c) 2021 Samson Achiaga
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.certified.schoolnotes.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.certified.schoolnotes.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.certified.schoolnotes.ui.theme.SchoolNotesTheme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class MainActivity : AppCompatActivity() {
-
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+@ExperimentalCoroutinesApi
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            SchoolNotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, number: Int) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        for (i in 0..number)
+            Surface {
+                Text(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily.Monospace,
+                    text = "Hello $name!",
+                    modifier = Modifier.padding(dimensionResource(id = com.intuit.sdp.R.dimen._16sdp))
+                )
+            }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SchoolNotesTheme {
+        Greeting("Android", 5)
     }
 }
---
+++ app/src/main/java/com/certified/schoolnotes/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.certified.schoolnotes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//val Purple200 = Color(0xFFBB86FC)
+//val Purple500 = Color(0xFF6200EE)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
+
+val colorPrimary = Color(0xFF3F51B5)
+val colorPrimaryDark = Color(0xFF303F9F)
+val colorAccent = Color(0xFFFF4081)
+val titleColor = Color(0xFF1F75FE)
+val titleColorAlpha = Color(0x551F75FE)
+val greyAlpha = Color(0x55999999)
+val separatorBackground = Color(0xFFAAAAAA)
+val separatorText = Color(0xFFFFFFFF)
