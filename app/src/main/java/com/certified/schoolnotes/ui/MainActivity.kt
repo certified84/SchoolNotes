67efec9400b139af6417a2026cc6541fec5c8544
Samson Achiaga <sirhumble84@gmail.com>
2022-03-28 04:16:03 +0100

    Made significant progress in the TodoScreen.kt design.

--- app/src/main/java/com/certified/schoolnotes/ui/MainActivity.kt
+++ app/src/main/java/com/certified/schoolnotes/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -18,9 +19,8 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.certified.schoolnotes.ui.screens.TodoScreen
 import com.certified.schoolnotes.ui.theme.SchoolNotesTheme
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-@ExperimentalCoroutinesApi
+@OptIn(ExperimentalMaterialApi::class)
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/certified/schoolnotes/ui/screens/TodoScreen.kt
+++ app/src/main/java/com/certified/schoolnotes/ui/screens/TodoScreen.kt
@@ -16,15 +16,18 @@
 
 package com.certified.schoolnotes.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,56 +36,135 @@ import com.certified.schoolnotes.model.Todo
 import com.certified.schoolnotes.ui.theme.SpaceGrotesk
 
 //@Preview(showBackground = true)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TodoScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.color_primary_accent))
+    ) {
 
-    Surface {
-        Column(
+        Text(
+            text = "To-Dos",
+            fontSize = 18.sp,
+            fontFamily = SpaceGrotesk,
+            color = colorResource(id = R.color.black_day_white_night),
+            fontWeight = FontWeight.Medium,
             modifier = Modifier
-                .fillMaxSize()
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(top = 16.dp)
+        )
+
+        Surface(
+            color = colorResource(id = R.color.color_primary),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(top = 8.dp)
         ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_sort_black_24dp),
+                    contentDescription = "Filter Icon",
+                    modifier = Modifier
+                        .padding(start = 24.dp)
+                        .align(Alignment.CenterVertically)
+                )
+                var text by remember { mutableStateOf("All") }
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_delete_icon_24dp),
+                    contentDescription = "Delete icon",
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(end = 24.dp)
+                        .clickable {
 
-            Text(
-                text = "To-Dos",
-                fontSize = 18.sp,
-                fontFamily = SpaceGrotesk,
-                color = colorResource(id = R.color.black_day_white_night),
-                fontWeight = FontWeight.Medium,
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(top = 16.dp)
-            )
-
-            Surface(
-                color = colorResource(id = R.color.color_primary),
-                modifier = Modifier.fillMaxWidth().height(60.dp).padding(top = 8.dp)
-            ) {
-                Row {
-//                    Icon(
-//                        painter = painterResource(id = R.drawable.ic_todo_circle),
-//                        contentDescription = "Todo circle"
-//                    )
-                    var text by remember { mutableStateOf("All") }
-                    TextField(
-                        value = text,
-                        onValueChange = { text = it },
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
-//                    Icon(
-//                        painter = painterResource(id = R.drawable.ic_delete_icon_24dp),
-//                        contentDescription = "Delete icon"
-//                    )
-                }
+                        }
+                )
             }
+        }
+
+        ListTodos(
+            todos = listOf(
+                Todo(
+                    id = 0,
+                    todo = "Complete these mf designs",
+                    isDone = false
+                ),
+                Todo(
+                    id = 1,
+                    todo = "Complete these mf designs",
+                    isDone = false
+                ),
+                Todo(
+                    id = 2,
+                    todo = "Complete these mf designs",
+                    isDone = false
+                ),
+                Todo(
+                    id = 3,
+                    todo = "Complete these mf designs",
+                    isDone = false
+                ),
+                Todo(
+                    id = 4,
+                    todo = "Complete these mf designs",
+                    isDone = false
+                )
+            )
+        )
+    }
+}
+
+@Composable
+fun ListTodos(todos: List<Todo>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items = todos, key = {
+            it.id
+        }) { it ->
+            TodoItem(todo = it)
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun TodoItem(todo: Todo, onClick: () -> Unit) {
-    Surface(onClick = onClick) {
-        Column {
+fun TodoItem(todo: Todo) {
+    Column(modifier = Modifier.background(color = colorResource(id = R.color.white))) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_todo_eclipse_10dp),
+                contentDescription = "Todo icon",
+                modifier = Modifier
+                    .padding(start = 24.dp, top = 4.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Text(
+                text = todo.todo,
+                fontSize = 14.sp,
+                fontFamily = SpaceGrotesk,
+                color = colorResource(id = R.color.black_day_white_night),
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 4.dp)
+                    .align(alignment = Alignment.CenterVertically),
+                maxLines = 1
+            )
+            var isChecked by remember { mutableStateOf(todo.isDone) }
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = { isChecked = it },
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+        Row {
 
         }
     }
