c1374b4757a47dfe58e18286ac263afe726ed7a5
Samson Achiaga <sirhumble84@gmail.com>
2022-03-28 02:01:22 +0100

    Fixed bug with font family and weight

--- app/src/main/java/com/certified/schoolnotes/ui/theme/Type.kt
+++ app/src/main/java/com/certified/schoolnotes/ui/theme/Type.kt
@@ -1,18 +1,47 @@
 package com.certified.schoolnotes.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.certified.schoolnotes.R
 
 // Set of Material typography styles to start with
+private val light = Font(R.font.space_grotesk_light, FontWeight.W300)
+private val regular = Font(R.font.space_grotesk_regular, FontWeight.W400)
+private val medium = Font(R.font.space_grotesk_medium, FontWeight.W500)
+private val semibold = Font(R.font.space_grotesk_semi_bold, FontWeight.W600)
+private val bold = Font(R.font.space_grotesk_bold, FontWeight.W700)
+
+val SpaceGrotesk = FontFamily(light, regular, medium, semibold, bold)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+    defaultFontFamily = SpaceGrotesk,
+//    body1 = TextStyle(
+//        fontFamily = SpaceGrotesk,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp
+//    ),
+//    h1 = TextStyle(
+//        fontFamily = SpaceGrotesk,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 24.sp
+//    ),
+//    h2 = TextStyle(
+//        fontFamily = SpaceGrotesk,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 20.sp
+//    ),
+//    h3 = TextStyle(
+//        fontFamily = SpaceGrotesk,
+//        fontWeight = FontWeight.SemiBold,
+//        fontSize = 20.sp
+//    ),
+//    h4 = TextStyle(
+//        fontFamily = SpaceGrotesk,
+//        fontWeight = FontWeight.SemiBold,
+//        fontSize = 18.sp
+//    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
