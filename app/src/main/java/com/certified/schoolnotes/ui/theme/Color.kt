d623f47ae71d183ef85da08112ad885c008514fb
Samson Achiaga <sirhumble84@gmail.com>
2022-04-28 06:51:09 +0100

    Added some colors to handle dark theme in future.

--- app/src/main/java/com/certified/schoolnotes/ui/theme/Color.kt
+++ app/src/main/java/com/certified/schoolnotes/ui/theme/Color.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 Samson Achiaga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.certified.schoolnotes.ui.theme
 
 import androidx.compose.ui.graphics.Color
@@ -7,9 +23,38 @@ import androidx.compose.ui.graphics.Color
 //val Purple700 = Color(0xFF3700B3)
 //val Teal200 = Color(0xFF03DAC5)
 
-val colorPrimary = Color(0xFF3F51B5)
-val colorPrimaryDark = Color(0xFF303F9F)
-val colorAccent = Color(0xFFFF4081)
+// Day colors
+val colorPrimary = Color(0xFFEFB8C8)
+val colorPrimaryDark = Color(0xFFD29DAC)
+val colorPrimaryDarker = Color(0xFFD29DAC)
+val colorAccent = Color(0xFFFFECF1)
+val colorAccent2 = Color(0xFFFFD8E4)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+val blackDayWhiteNight = Color(0xFF000000)
+val whiteDayBlackNight = Color(0xFFFFFFFF)
+val colorSearchBar = Color(0xFFE1E1E1)
+val fragmentBackground = Color(0xFFFFECF1)
+val results = Color(0xFF986977)
+val buttonColor = Color(0xFFEFB8C8)
+val red  = Color(0xFFED1B35)
+
+// Night colors
+val colorPrimaryNight = Color(0xFFEFB8C8)
+val colorPrimaryDarkNight = Color(0xFFD29DAC)
+val colorPrimaryDarkerNight = Color(0xFFD29DAC)
+val colorAccentNight = Color(0xFFFFECF1)
+val colorAccent2Night = Color(0xFFFFD8E4)
+val blackNight = Color(0xFF000000)
+val whiteNight = Color(0xFFFFFFFF)
+val blackDayWhiteNightNight = Color(0xFF000000)
+val whiteDayBlackNightNight = Color(0xFFFFFFFF)
+val colorSearchBarNight = Color(0xFFE1E1E1)
+val fragmentBackgroundNight = Color(0xFF313033)
+val resultsNight = Color(0xFF986977)
+val buttonColorNight = Color(0xFF1C1B1F)
+val redNight  = Color(0xFFED1B35)
+
 val titleColor = Color(0xFF1F75FE)
 val titleColorAlpha = Color(0x551F75FE)
 val greyAlpha = Color(0x55999999)
